8bd1b2d0c95fbaa814edf4428c94471c95e0309a
agiratech-taham <taha.murtaza@agiratech.com>
2022-11-22 19:39:11 +0530

    Todo List UI

---
+++ src/components/TodoModal.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import styles from "../styles/modules/modal.module.scss";
+import { MdOutlineClose } from "react-icons/md";
+import Button from "./Button";
+
+function TodoModal({ modalOpen, setModalOpen }) {
+  const [title, setTitle] = useState("");
+  const [Status, setStatus] = useState("");
+  return (
+    // <div>
+    modalOpen && (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+            tabIndex={0}
+            role="button"
+          >
+            <MdOutlineClose />
+          </div>
+          <form className={styles.form}>
+            <h1 className={styles.formTitle}>Add Task</h1>
+            <label htmlFor="title">
+              Title
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
+            <label htmlFor="type">
+              Status
+              <select id="type">
+                <option value="incomplete">Incomplete</option>
+                <option value="complete">Completed</option>
+              </select>
+            </label>
+            <div className={styles.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Task
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+          {/* <h1>TodoModal</h1> */}
+        </div>
+      </div>
+    )
+    // </div>
+  );
+}
+
+export default TodoModal;
